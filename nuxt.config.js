38a9fdc049424eddc3578b1656a5f9981280de91
Devessier <baptiste@devessier.fr>
2019-09-17 23:21:12 +0200

    feat(apple): tell to Apple our app is webapp capable !

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,14 @@ module.exports = {
                 hid: 'description',
                 name: 'description',
                 content: process.env.npm_package_description || ''
+            },
+            {
+                name: 'apple-mobile-web-app-capable',
+                content: 'yes'
+            },
+            {
+                name: 'apple-mobile-web-app-status-bar-style',
+                content: 'black-translucent'
             }
         ],
         link: [
